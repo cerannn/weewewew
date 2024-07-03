552a5c807def8a60ef68f0590751d856e6166271
cerannn <cerannemir@gmail.com>
2024-07-03 16:53:36 +0300

    Add files via upload

---
+++ src/images/index.ts
@@ -0,0 +1,19 @@
+import hamsterExchange from "./hamster-exchange.png";
+import binanceLogo from "./binance-logo.png";
+import dollarCoin from "./dollar-coin.png";
+import dailyReward from "./daily-reward.png";
+import dailyCipher from "./daily-cipher.png";
+import dailyCombo from "./daily-combo.png";
+import mainCharacter from "./main-character.png";
+import kenshiCoin from "./kenshi-coin.png";
+
+export {
+    hamsterExchange,
+    binanceLogo,
+    dollarCoin,
+    dailyReward,
+    dailyCipher,
+    dailyCombo,
+    mainCharacter,
+    kenshiCoin
+};
